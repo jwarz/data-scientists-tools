73c333183559f5d28a6dfa132a5db4a9acb9dfd9
jwarz <joschka.schwarz@tuhh.de>
2022-05-06 22:58:09 +0200

    img style

--- create_hexsticker.R
+++ create_hexsticker.R
@@ -1,7 +1,7 @@
 sysfonts::font_add_google("Raleway", "pf", regular.wt = 500)
 hexSticker::sticker(
   subplot = ~ plot.new(), s_x = 1, s_y = 1, s_width = 0.1, s_height = 0.1,
-  package = "class", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
-  p_color = "#3F8CCC", h_fill = "#EBE4C4", h_color = "#3F8CCC",
-  dpi = 320, filename = "~/logo_test.png"
+  package = "pROC", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
+  p_color = "#2dc6d6", h_fill = "#EBE4C4", h_color = "#2dc6d6",
+  dpi = 320, filename = "~/03_repos/data-scientists-tools/img/logo_test.png"
 )
