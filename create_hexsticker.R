4d2248917a3872fb10d8f8310160c54d999fb656
jwarz <joschka.schwarz@tuhh.de>
2022-05-10 17:52:41 +0200

    img

--- create_hexsticker.R
+++ create_hexsticker.R
@@ -1,16 +1,15 @@
 sysfonts::font_add_google("Raleway", "pf", regular.wt = 500)
 hexSticker::sticker(
   subplot = ~ plot.new(), s_x = 1, s_y = 1, s_width = 0.1, s_height = 0.1,
-  package = "ranger", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
+  package = "cluster", p_x = 1, p_y = 1, p_size = 8, h_size = 1.2, p_family = "pf",
   p_color = "#2dc6d6", h_fill = "transparent", h_color = "#2dc6d6",
-  dpi = 320, filename = "~/03_repos/data-scientists-tools/img/hex_ranger.png"
+  dpi = 320, filename = "~/03_repos/data-scientists-tools/img/hex_cluster.png"
 )
 
 
 
-imgurl <- system.file("~/03_repos/data-scientists-tools/img/logo_vtreat.png", package="hexSticker")
-imgurl <- "~/03_repos/data-scientists-tools/img/logo_xgboost.png"
+imgurl <- "~/03_repos/data-scientists-tools/img/carrot.png"
 
-hexSticker::sticker(imgurl, package=NULL, p_size=8, s_x=1, s_y=.75, s_width=.6,
+hexSticker::sticker(imgurl, package="caret", p_size=8, s_x=1, s_y=.75, s_width=.6,
                     p_color = "#2dc6d6", h_fill = "transparent", h_color = "#2dc6d6",
-        filename="~/03_repos/data-scientists-tools/img/hex_xgboost.png")
+        filename="~/03_repos/data-scientists-tools/img/hex_caret.png")
